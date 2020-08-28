97abb6fc823e567952cdaf2c3593c3ba9c38626b
Joaquin Romera <joakromera@gmail.com>
2020-08-28 12:13:39 -0300

    Refactoring Record Tests

--- tests/record_test.cpp
+++ tests/record_test.cpp
@@ -2,64 +2,82 @@
 #include "../src/Datum.h"
 #include "../src/Record.h"
 
-using namespace std;
-
-TEST(registro_test, generadores)
+TEST(RecordTests, Construction)
 {
   Record({ "LU", "LU_A", "Nombre", "Carrera" }, { Datum(182), Datum(18), Datum("March"), Datum("Computacion") });
   Record({}, {});
 }
 
-TEST(registro_test, campos)
+TEST(RecordTests, ColumnsAreCorrectlySet)
 {
   Record r({ "A", "B", "C" }, { Datum(1), Datum(2), Datum(3) });
-  Record r2({}, {});
+  linear_set<std::string>::size_type expectedSize = 3;
+  EXPECT_EQ(expectedSize, r.columns().size());
+  EXPECT_EQ(r.columns(), linear_set<std::string>({ "A", "B", "C" }));
 
-  EXPECT_EQ(r.columns(), linear_set<string>({ "A", "B", "C" }));
-  EXPECT_EQ(r2.columns(), linear_set<string>());
+  Record r2({}, {});
+  expectedSize = 0;
+  EXPECT_EQ(expectedSize, r2.columns().size());
+  EXPECT_EQ(r2.columns(), linear_set<std::string>());
 }
 
-TEST(registro_test, dato)
+TEST(RecordTests, DataTypeAndValueAreCorrect)
 {
   Record r({ "LU", "LU_A", "Nombre", "Carrera" }, { Datum(182), Datum(18), Datum("March"), Datum("Computacion") });
 
   EXPECT_EQ(r.value("LU").isInteger(), true);
   EXPECT_EQ(r.value("LU").isString(), false);
   EXPECT_EQ(r.value("LU").value<int>(), 182);
+
   EXPECT_EQ(r.value("Nombre").isInteger(), false);
   EXPECT_EQ(r.value("Nombre").isString(), true);
   EXPECT_EQ(r.value("Nombre").value<std::string>(), "March");
 }
 
-TEST(registro_test, ig_obs)
+TEST(RecordTests, TwoRecordsWithSameColumnsAndValuesAreEqual)
 {
   Record r1({ "LU", "LU_A" }, { Datum(1), Datum("a") });
   Record r2({ "LU", "LU_A" }, { Datum(1), Datum("a") });
-
-  // Iguales
   EXPECT_TRUE(r1 == r2);
-
-  // Distinto orden
-  Record r3({ "LU_A", "LU" }, { Datum("a"), Datum(1) });
-  EXPECT_TRUE(r1 == r3);
-
-  // Distinto tipo
-  Record r4({ "LU_A", "LU" }, { Datum(2), Datum(1) });
-  EXPECT_FALSE(r1 == r4);
-
-  // Distinto valor
-  Record r5({ "LU", "LU_A" }, { Datum(2), Datum("a") });
-  EXPECT_FALSE(r1 == r5);
-
-  // Más columns
-  Record r6({ "LU", "LU_A", "Nombre" },
-    { Datum(2), Datum("a"), Datum("b") });
-  EXPECT_FALSE(r1 == r6);
 }
 
-TEST(registro_test, copy)
+TEST(RecordTests, TwoRecordsWithSameColumnsAndValuesAreEqualDespiteColumnOrder)
+{
+  Record r1({ "LU", "LU_A" }, { Datum(1), Datum("a") });
+  Record r3({ "LU_A", "LU" }, { Datum("a"), Datum(1) });
+  EXPECT_TRUE(r1 == r3);
+}
+
+TEST(RecordTests, TwoRecordsWithSameColumnsWithDifferentTypesAreNotEqual)
+{
+  Record r1({ "LU", "LU_A" }, { Datum(1), Datum("a") });
+  Record r4({ "LU_A", "LU" }, { Datum(2), Datum(1) });
+  EXPECT_FALSE(r1 == r4);
+}
+
+TEST(RecordTests, TwoRecordsWithSameColumnsAndDifferentValuesAreNotEqual)
+{
+  Record r1({ "LU", "LU_A" }, { Datum(1), Datum("a") });
+  Record r5({ "LU", "LU_A" }, { Datum(2), Datum("a") });
+  Record r6({ "LU", "LU_A" }, { Datum(1), Datum("") });
+  EXPECT_FALSE(r1 == r5);
+  EXPECT_FALSE(r1 == r6);
+  EXPECT_FALSE(r5 == r6);
+}
+
+TEST(RecordTests, TwoRecordsWithDifferentColumnSizeAreNotEqual)
+{
+  Record r1({ "LU", "LU_A" }, { Datum(1), Datum("a") });
+  Record r7({ "LU", "LU_A", "Nombre" }, { Datum(2), Datum("a"), Datum("b") });
+  Record r8({ "LU" }, { Datum(2) });
+  EXPECT_FALSE(r1 == r7);
+  EXPECT_FALSE(r1 == r8);
+  EXPECT_FALSE(r7 == r8);
+}
+
+TEST(RecordTests, TwoRecordsAreEqualWhenCopied)
 {
   Record r({ "A", "B", "C" }, { Datum(1), Datum(2), Datum(3) });
-  auto copy_r(r);
-  EXPECT_EQ(copy_r, r);
+  auto recordCopy(r);
+  EXPECT_EQ(recordCopy, r);
 }
